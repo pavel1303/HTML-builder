f6703bfd2a0644a0c08b4e9281bc581c3eb90b78
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-05-23 23:59:31 +0300

    feat: make 6st task, and end task HTML-builder

---
+++ 06-build-page/index.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const promiseFs = require('fs/promises');
+const fs = require('fs');
+
+async function buildHTML(){
+  await promiseFs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true});
+  await promiseFs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), '');
+  //Файл с шаблоном HTML
+  let sampleHTML = await promiseFs.readFile(path.join(__dirname,'template.html'), 'utf-8');
+  //Массив с компонентами
+  const components = await promiseFs.readdir(path.join(__dirname, 'components'));
+  //Массив с названиями тегов-шаблонов
+  const componentsName = [];
+  //Прохожусь по файлу с шаблоном и ищу теги, которые необходимо заменить на компонент
+  sampleHTML.split(' ').forEach(elem => {
+    let cur = findComponent(elem);
+    if(cur !== ''){
+      componentsName.push(cur);
+    }
+  });
+  // Создаю объект с контеном компонентов
+  let objContent ={};
+  await createObjComponents(componentsName, objContent);
+  //Собираю новый HTML файл
+  //Разбиваю шаблон на строки
+  sampleHTML.split('\n').forEach(async (el) => {
+    //Если строка содержит {{, то вместо строки вставляем компонент
+    if(el.includes('{{')){
+      //Выбираем подходящий компонент
+      componentsName.forEach(async (component) => {
+        let tag = '{{'+`${component}`+'}}';
+        if(components.includes(`${component}.html`) && el.includes(tag)){
+          await promiseFs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), '\n'+objContent[component] + '\n');
+        }
+      });
+    }else{
+      await promiseFs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), el);
+    }
+  });
+}
+//Вызываю функцию копирования для папки assets
+const dirForCopy = path.join(__dirname, 'assets');
+const targetDir = path.join(__dirname, 'project-dist', 'assets');
+copyDir(dirForCopy, targetDir);
+//Создаю и наполняю index.html
+buildHTML();
+//Вызываю функцию сбора стилей и добавления в проект
+const styles = path.join(__dirname, 'styles');
+const dist = path.join(__dirname, 'project-dist');
+buildStyles(styles, dist);
+
+
+
+//Функчия для поиска компонентов в шаблоне
+function findComponent(elem){
+  let component = '';
+  if(elem.startsWith('{{')){
+    let stop = false;
+    elem.split('').forEach(char => {
+      if(char === '{'){
+        component += '';
+      } else if (char === '}'){
+        stop = true;
+      } else{
+        if(!stop){
+          component += char;
+        }
+      }
+    });
+  }
+  return component;
+}
+// Функция для создания объекта с контентом компонентов
+async function createObjComponents(names,obj){
+  for(let name of names){
+    obj[name] = await promiseFs.readFile(path.join(__dirname, 'components', `${name}.html`));
+  }
+}
+//функция для сбора стилей в один бандл
+async function buildStyles(stylesDir, build){
+  const files = await promiseFs.readdir(stylesDir, {withFileTypes: true});
+  await promiseFs.writeFile(path.join(build, 'style.css'), '');
+  const output = fs.createWriteStream(path.join(build, 'style.css'));
+  files.forEach(file => {
+    const input = fs.createReadStream(path.join(stylesDir, file.name));
+    const ext = path.extname(path.join(styles, file.name));
+    if(file.isFile() && ext === '.css'){
+      input.pipe(output);
+    }
+  });
+}
+
+//Функция копирования
+
+async function copyDir(toCopy, copyHere){
+  if(copyHere){
+    await promiseFs.rm(copyHere, { recursive: true, force: true });
+  }
+  await promiseFs.mkdir(copyHere, {recursive: true});
+  const files = await promiseFs.readdir(toCopy,{withFileTypes: true});
+  files.forEach(file =>{
+    if(!file.isDirectory()){
+      promiseFs.copyFile(path.join(toCopy, file.name), path.join(copyHere, file.name));
+    } else if(file.isDirectory()){
+      copyDir(path.join(toCopy, file.name), path.join(copyHere, file.name));
+    }
+  });
+}
